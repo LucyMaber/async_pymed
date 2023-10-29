3ff6bf2e20e9f44b813f4afe12f83dba4e693e87
Lucy (elf gold) <maberwilliam@gmail.com>
2023-10-29 12:30:35 +0000

    Update main.py

--- examples/simple_search/main.py
+++ examples/simple_search/main.py
@@ -1,23 +1,24 @@
+import asyncio
 from pymed import PubMed
 
+async def fetch_pubmed_data():
+    # Create a PubMed object that GraphQL can use to query
+    pubmed = PubMed(tool="MyTool", email="my@email.address")
 
-# Create a PubMed object that GraphQL can use to query
-# Note that the parameters are not required but kindly requested by PubMed Central
-# https://www.ncbi.nlm.nih.gov/pmc/tools/developers/
-pubmed = PubMed(tool="MyTool", email="my@email.address")
+    # Create a GraphQL query in plain text
+    query = "occupational health[Title]"
 
-# Create a GraphQL query in plain text
-query = "occupational health[Title]"
+    # Execute the query against the API asynchronously
+    results = await pubmed.query(query, max_results=500)
 
+    # Loop over the retrieved articles
+    for article in results:
 
-# Execute the query against the API
-results = pubmed.query(query, max_results=500)
+        # Print the type of object we've found (can be either PubMedBookArticle or PubMedArticle)
+        print(type(article))
 
-# Loop over the retrieved articles
-for article in results:
+        # Print a JSON representation of the object
+        print(article.toJSON())
 
-    # Print the type of object we've found (can be either PubMedBookArticle or PubMedArticle)
-    print(type(article))
-
-    # Print a JSON representation of the object
-    print(article.toJSON())
+# Run the async function
+asyncio.run(fetch_pubmed_data())
